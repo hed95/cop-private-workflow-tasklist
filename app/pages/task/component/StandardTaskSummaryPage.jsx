6411b866df4b5046bdb0f27143b0b7749fb8b43c
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-10-04 08:38:55 +0100

    added comments for details

--- app/pages/task/component/StandardTaskSummaryPage.jsx
+++ app/pages/task/component/StandardTaskSummaryPage.jsx
@@ -4,16 +4,27 @@ import moment from "moment/moment";
 import {priority} from "../../../core/util/priority";
 import TaskTitle from "./TaskTitle";
 import {DataSpinner} from "../../../core/components/DataSpinner";
+import TaskForm from "../../../core/task-form/components/TaskForm";
+import Comments from "./Comments";
 
 
 class StandardTaskSummaryPage extends React.Component {
 
     render() {
         const {task, variables} = this.props;
-        return <div style={{'paddingTop': '5px'}}>
+        return <div>
             <TaskTitle {...this.props} />
-            <p>{task.get('description')}</p>
-            <Actions task={task} variables={variables}/>
+            <div className="grid-row">
+                <div className="column-two-thirds" style={{paddingTop: '10px'}}>
+                    <p>{task.get('description')}</p>
+                    <Actions task={task} variables={variables}/>
+                </div>
+                <div className="column-one-third" style={{paddingTop: '10px'}}>
+                    <Comments taskId={task.get("id")}/>
+                </div>
+            </div>
+
+
         </div>
     }
 }
