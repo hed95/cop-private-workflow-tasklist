2a4b054c61c8c8af7df7e08763b090f9cc514db7
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-10-25 12:38:04 +0100

    updated

--- app/core/components/SubmissionBanner.jsx
+++ app/core/components/SubmissionBanner.jsx
@@ -1,38 +1,50 @@
-import React from "react";
+import React from 'react';
 import PubSub from 'pubsub-js';
 
 
 class SubmissionBanner extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {submission: false, message: null,  visibility: 'hidden'};
-        PubSub.subscribe('submission', (msg, data) => {
-            this.setState({
-                submission: data.submission,
-                message: data.message
-            });
-            setTimeout(() => {
-                this.setState({
-                    submission: false, message: null
-                })
-            }, 5000);
-        });
+  constructor() {
+    super();
+    this.state = { submission: false, message: null, visibility: 'hidden' };
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  componentWillMount() {
+    this.token = PubSub.subscribe('submission', this.handleSubmission);
+  }
+
+
+  componentWillUnmount() {
+    if (this.token) {
+      PubSub.unsubscribe(this.token);
     }
+  }
 
+  handleSubmission(mgs, data) {
+    this.setState({
+      submission: data.submission,
+      message: data.message
+    });
+    setTimeout(() => {
+      this.setState({
+        submission: false, message: null
+      });
+    }, 5000);
+  }
 
-    render() {
-        const {submission, message} = this.state;
+  render() {
+    const { submission, message } = this.state;
 
-        return submission ? <div className="container">
-                <div className="govuk-box-highlight confirm-page new">
-                    <span className="hod-checkmark"/>
-                    <h2 className="heading-small">
-                        {message}
-                    </h2>
-                </div>
-            </div> : <div/>
-    }
+    return submission ? <div className="container">
+      <div className="govuk-box-highlight confirm-page new">
+        <span className="hod-checkmark"/>
+        <h2 className="heading-small">
+          {message}
+        </h2>
+      </div>
+    </div> : <div/>;
+  }
 }
 
 export default SubmissionBanner;
