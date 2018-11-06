d064c76de3f256a6388ac7d49389a899513b2469
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-11-06 12:36:52 +0000

    fixing issue with comments

--- app/pages/task/component/CreateComment.jsx
+++ app/pages/task/component/CreateComment.jsx
@@ -1,60 +1,62 @@
-import React, {PropTypes} from "react";
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import * as actions from "../actions";
-import {createStructuredSelector} from "reselect";
-import {form, isFetchingCreateCommentForm} from "../selectors";
-import {Form} from 'react-formio'
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { createStructuredSelector } from 'reselect';
+import { form, isFetchingCreateCommentForm } from '../selectors';
+import { Form } from 'react-formio';
+import AppConstants from '../../../common/AppConstants';
 
 class CreateComment extends React.Component {
 
-    componentDidMount() {
-        this.props.fetchCreateCommentForm();
+  componentDidMount() {
+    this.props.fetchCreateCommentForm();
+  }
+
+  shouldComponentUpdate() {
+    return !this.form;
+  }
+
+  renderForm() {
+    const { isFetchingCreateCommentForm, form} = this.props;
+    if (isFetchingCreateCommentForm) {
+      return <div>Loading....</div>;
     }
 
-    renderForm() {
-        const {isFetchingCreateCommentForm, form} = this.props;
-        if (isFetchingCreateCommentForm) {
-            return <div>Loading....</div>
-        } else {
-
-            const options = {
-                noAlerts: true
-            };
-            if (form) {
-                return <div><Form form={form} options={options}
-                               ref={(form) => this.form = form}
-                               onSubmit={(submission) =>{
-                                   this.props.createComment({taskId: this.props.taskId, comment: submission.data});
-                                   this.form.formio.emit('submitDone');
-                                   this.form.formio.resetValue();
-                                   this.form.formio.render();
-
-                               }}/></div>
-            } else {
-                return <div/>
-            }
-        }
+    const options = {
+      noAlerts: true,
+    };
+    if (form) {
+      return (<div><Form
+        form={form} options={options}
+        ref={form => this.form = form}
+        onSubmit={(submission) => {
+          this.props.createComment({ taskId: this.props.taskId, comment: submission.data });
+          this.form.formio.emit('submitDone');
+          this.form.formio.resetValue();
+        }}
+      /></div>);
     }
+    return <div />;
+  }
 
-    render() {
-        return <div id="commentContainer">
-            {this.renderForm()}
-        </div>
-
-    }
+  render() {
+    return (<div id="commentContainer">
+      {this.renderForm()}
+    </div>);
+  }
 }
 
 
 CreateComment.propTypes = {
-    fetchCreateCommentForm: PropTypes.func.isRequired,
-    createComment: PropTypes.func.isRequired,
-    isFetchingCreateCommentForm: PropTypes.bool,
+  fetchCreateCommentForm: PropTypes.func.isRequired,
+  createComment: PropTypes.func.isRequired,
+  isFetchingCreateCommentForm: PropTypes.bool
 };
 
 const mapStateToProps = createStructuredSelector({
-    isFetchingCreateCommentForm: isFetchingCreateCommentForm,
-    form: form,
+  isFetchingCreateCommentForm,
+  form
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
