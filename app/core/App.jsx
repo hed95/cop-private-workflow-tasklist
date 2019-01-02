838a6b0b3f2f46b9fd11d2e76feb85b9c54ec5c6
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2019-01-02 11:38:37 +0000

    updating app

--- app/core/App.jsx
+++ app/core/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Footer from './components/Footer'
-import Main from './Main'
-import Header from "./components/Header";
-import SubmissionBanner from "./components/SubmissionBanner";
+import React, { lazy, Suspense } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import DataSpinner from '../core/components/DataSpinner';
+
+const Main = lazy(() => import('./Main'));
+const SubmissionBanner = lazy(() => import('../core/components/SubmissionBanner'));
 
 const App = () => (
-    <div>
-        <Header/>
-        <SubmissionBanner />
-        <div className="container" style={{height:'100vh'}}>
-            <Main/>
-        </div>
-        <Footer/>
+  <div>
+    <Header/>
+    <SubmissionBanner/>
+    <div className="container" style={{ height: '100vh' }}>
+      <Suspense
+        fallback={<div style={{ display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
+          <DataSpinner message="Loading main..."/></div>}>
+        <Main/>
+      </Suspense>
     </div>
+    <Footer/>
+  </div>
 );
 
-export default App
+export default App;
