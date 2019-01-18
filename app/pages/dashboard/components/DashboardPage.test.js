e045378dd59c5a0320428bda38aee249e12e64a4
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2019-01-18 10:38:18 +0000

    fixing build

--- app/pages/dashboard/components/DashboardPage.test.js
+++ app/pages/dashboard/components/DashboardPage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow} from 'enzyme';
 import configureStore from 'redux-mock-store';
 import {DashboardPage} from './DashboardPage';
-
+import Immutable from 'immutable'
 
 describe('DashboardPage', () => {
   const mockStore = configureStore();
@@ -14,72 +14,27 @@ describe('DashboardPage', () => {
     store = mockStore(initialState);
   });
   const resetErrors = jest.fn();
-  const fetchActiveShift = jest.fn();
 
-  it('renders data spinner checking for shift', () => {
-    const props = {
-      isFetchingShift: true
-    };
 
-    const wrapper = shallow(<DashboardPage
-      store={store}
-      {...props}
-      fetchActiveShift={fetchActiveShift}
-      resetErrors ={resetErrors}
-    />);
-
-    expect(resetErrors).toBeCalled();
-    expect(fetchActiveShift).toBeCalled();
-
-    expect(wrapper.find('DataSpinner').exists()).toEqual(true);
-    expect(wrapper.find('#dashboardContent').exists()).toEqual(false);
-  });
   it('renders panels if shift present', async() => {
     const props = {
       isFetchingShift: false,
-      hasActiveShift: true
+      hasActiveShift: true,
+      shift: Immutable.fromJS({
+        shiftid: 'shiftid'
+      })
     };
 
     const wrapper = shallow(<DashboardPage
       store={store}
       {...props}
-      fetchActiveShift={fetchActiveShift}
       resetErrors ={resetErrors}
     />);
 
     expect(resetErrors).toBeCalled();
-    expect(fetchActiveShift).toBeCalled();
 
-    expect(wrapper.find('#dataSpinner').exists()).toEqual(false);
     expect(wrapper.find('ErrorPanel')).toBeDefined();
     expect(wrapper.find('DashboardTitle')).toBeDefined();
     expect(wrapper.find('DashboardPanel')).toBeDefined();
-  });
-  it('renders panels and warning if shift not present', async() => {
-    const props = {
-      isFetchingShift: false,
-      hasActiveShift: false,
-      location: {
-        pathname: "/dashboard"
-      }
-    };
-
-    const wrapper = shallow(<DashboardPage
-      store={store}
-      {...props}
-      fetchActiveShift={fetchActiveShift}
-      resetErrors ={resetErrors}
-    />);
-
-    expect(resetErrors).toBeCalled();
-    expect(fetchActiveShift).toBeCalled();
-
-    expect(wrapper.find('#dataSpinner').exists()).toEqual(false);
-    expect(wrapper.find('ErrorPanel')).toBeDefined();
-    expect(wrapper.find('DashboardTitle')).toBeDefined();
-    expect(wrapper.find('DashboardPanel')).toBeDefined();
-
-    expect(wrapper.find('.notice').exists()).toEqual(true);
-    expect(wrapper.find('.notice').text()).toEqual('WarningPlease start your shift before proceeding')
   });
 });
