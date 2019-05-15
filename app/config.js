35066b3079598faae1011b95e7d1049c6960f2f6
shepda <david@x-designs.co.uk>
2019-05-15 15:03:42 +0100

    Update service urls

--- app/config.js
+++ app/config.js
@@ -11,19 +11,19 @@ const {
 const config = {
   services: {
     operationalData: {
-      url: `${PROTOCOL}://${OPERATIONAL_POSTGREST_NAME}.${EXT_DOMAIN}`,
+      url: `${PROTOCOL}${OPERATIONAL_POSTGREST_NAME}.${EXT_DOMAIN}`,
     },
     workflow: {
-      url: `${PROTOCOL}://${WORKFLOW_SERVER_NAME}.${EXT_DOMAIN}`,
+      url: `${PROTOCOL}${WORKFLOW_SERVER_NAME}.${EXT_DOMAIN}`,
     },
     translation: {
-      url: `${PROTOCOL}://${TRANSLATION_SERVER_NAME}.${EXT_DOMAIN}`,
+      url: `${PROTOCOL}${TRANSLATION_SERVER_NAME}.${EXT_DOMAIN}`,
     },
     form: {
-      url: `${PROTOCOL}://${FORMIO_SERVER_NAME}.${EXT_DOMAIN}`,
+      url: `${PROTOCOL}${FORMIO_SERVER_NAME}.${EXT_DOMAIN}`,
     },
     report: {
-      url: `${PROTOCOL}://${REPORTING_SERVER_NAME}.${EXT_DOMAIN}`,
+      url: `${PROTOCOL}${REPORTING_SERVER_NAME}.${EXT_DOMAIN}`,
     },
   },
 };
