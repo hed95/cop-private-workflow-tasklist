4abf46b70ebe75f3b8aa84c37647a74abe8f958f
Mark Olliver <mark@digitalpatterns.io>
2019-06-07 09:46:20 +0100

    test fixing comments

--- app/pages/task/display/component/Comments.jsx
+++ app/pages/task/display/component/Comments.jsx
@@ -11,6 +11,9 @@ import moment from "moment";
 import Pagination from "../../../../core/components/Pagination";
 import ShowMore from 'react-show-more';
 import Collapsible from 'react-collapsible';
+import {withRouter} from "react-router";
+import {yourTasks} from "../../../tasks/selectors";
+import {YourTasksContainer} from "../../../tasks/components/YourTasksContainer";
 
 const uuidv4 = require('uuid/v4');
 
@@ -98,11 +101,12 @@ Comments.propTypes = {
     appConfig: PropTypes.object,
 };
 
-const mapStateToProps = createStructuredSelector({
-    isFetchingComments: isFetchingComments,
-    comments: comments,
-});
-
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Comments);
+export default connect((state) => {
+    return {
+        isFetchingComments: isFetchingComments(state),
+        comments: comments(state),
+        appConfig: state.appConfig,
+    }
+}, mapDispatchToProps)(Comments);
