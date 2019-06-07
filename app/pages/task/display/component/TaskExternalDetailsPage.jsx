5b3a5d31f119af12b71314c77c0760671eff33f5
Mark Olliver <mark@digitalpatterns.io>
2019-06-07 10:36:38 +0100

    revert change put in correct file

--- app/pages/task/display/component/TaskExternalDetailsPage.jsx
+++ app/pages/task/display/component/TaskExternalDetailsPage.jsx
@@ -4,20 +4,3 @@ class TaskExternalDetailsPage extends React.Component {
     }
 }
 export default TaskExternalDetailsPage;
-
-TaskDetailsPage.propTypes = {
-    updateDueDate: PropTypes.func.isRequired,
-    submissionStatus: PropTypes.string,
-    customEventSubmissionStatus: PropTypes.string,
-};
-
-const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
-
-export default withRouter(connect((state) => {
-    return {
-        kc: state.keycloak,
-        submissionStatus: submissionStatus(state),
-        customEventSubmissionStatus: customEventSubmissionStatus(state),
-        appConfig: appConfig(state)
-    };
-}, mapDispatchToProps)(TaskDetailsPage));
