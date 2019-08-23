26ee5d7cfa307cc4306e8b5258fe78d3a7b4ee07
David Shepherd <david@x-designs.co.uk>
2019-08-23 17:02:26 +0100

    Only display the comments form for tasks that have explicitly set the displayCommentForm flag
    The comment form is not displayed by default and can be displayed by adding a hidden field to the top level of the form with a name of `displayCommentForm` and a value of `true`.

--- app/pages/task/display/component/TaskDetailsPage.test.js
+++ app/pages/task/display/component/TaskDetailsPage.test.js
@@ -9,7 +9,26 @@ const { Map, List } = Immutable;
 
 describe('TaskDetailsPage', () => {
   const mockStore = configureStore();
+  const updateDueDate = jest.fn();
+  const dueDate = moment();
+  const props = {
+    candidateGroups: Immutable.fromJS([
+      'teamA',
+    ]),
+    task: Immutable.fromJS({
+      name: 'test',
+      formKey: null,
+      description: 'test',
+      due: dueDate,
+      id: 'taskid',
+      priority: 1000,
+    }),
+    appConfig: {
+      workflowServiceUrl: 'http://localhost:9000',
+    },
+  };
   let store;
+
   beforeEach(() => {
     store = mockStore({
       'task-page': new Map({
@@ -17,25 +36,8 @@ describe('TaskDetailsPage', () => {
       }),
     });
   });
-  const updateDueDate = jest.fn();
+
   it('renders task if no form key', async () => {
-    const dueDate = moment();
-    const props = {
-      candidateGroups: Immutable.fromJS([
-        'teamA',
-      ]),
-      task: Immutable.fromJS({
-        name: 'test',
-        formKey: null,
-        description: 'test',
-        due: dueDate,
-        id: 'taskid',
-        priority: 1000,
-      }),
-      appConfig: {
-        workflowServiceUrl: 'http://localhost:9000',
-      },
-    };
     const wrapper = await mount(<TaskDetailsPage
       store={store}
       {...props}
@@ -47,7 +49,50 @@ describe('TaskDetailsPage', () => {
     expect(wrapper.find('#taskPriority').text()).toEqual('PriorityHigh');
     expect(wrapper.find('#taskTeams').text()).toEqual('TeamteamA');
     expect(wrapper.find('#taskDescription').text()).toEqual('test');
-    const dueDateInput = wrapper.find('#updateDueDate');
-    expect(dueDateInput.props().defaultValue).toEqual(dueDate.format('DD-MM-YYYY HH:mm'));
+  });
+
+  it('renders the comment form if displayCommentForm is true', async () => {
+    props.form = {
+      components: [{
+        key: 'displayCommentForm',
+        defaultValue: 'true'
+      }]
+    };
+    const wrapper = await mount(<TaskDetailsPage
+      store={store}
+      {...props}
+      updateDueDate={updateDueDate}
+    />);
+    console.log(wrapper.html());
+    expect(wrapper.exists('#updateDueDate')).toEqual(true);
+  });
+
+  it('does not render the comment form if displayCommentForm is false', async () => {
+    props.form = {
+      components: [{
+        key: 'displayCommentForm',
+        defaultValue: 'false'
+      }]
+    };
+    const wrapper = await mount(<TaskDetailsPage
+      store={store}
+      {...props}
+      updateDueDate={updateDueDate}
+    />);
+    console.log(wrapper.html());
+    expect(wrapper.exists('#updateDueDate')).toEqual(false);
+  });
+
+  it('does not render the comment form if displayCommentForm is undefined', async () => {
+    props.form = {
+      components: [{}]
+    };
+    const wrapper = await mount(<TaskDetailsPage
+      store={store}
+      {...props}
+      updateDueDate={updateDueDate}
+    />);
+    console.log(wrapper.html());
+    expect(wrapper.exists('#updateDueDate')).toEqual(false);
   });
 });
