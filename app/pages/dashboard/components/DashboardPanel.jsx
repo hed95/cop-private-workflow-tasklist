4199fbe25e1877ea9bba3560afba6470a3d3e42c
Pedro Miguel <pedro.miguel@live.co.uk>
2019-09-17 16:42:28 +0100

    Linting

--- app/pages/dashboard/components/DashboardPanel.jsx
+++ app/pages/dashboard/components/DashboardPanel.jsx
@@ -1,128 +1,131 @@
+import PubSub from 'pubsub-js';
 import React, {Suspense} from 'react';
-
-import {withRouter} from "react-router";
 import SockJS from 'sockjs-client';
+
 import { Client, Message } from '@stomp/stompjs';
-import {connect} from "react-redux";
-import PubSub from "pubsub-js";
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+
+// local imports
 import DataSpinner from '../../../core/components/DataSpinner';
-import {bindActionCreators} from 'redux';
 import withLog from '../../../core/error/component/withLog';
 
-const ProceduresDashboardPanel = React.lazy(() => import('./ProceduresDashboardPanel'));
-const ReportsDashboardPanel = React.lazy(() => import('./ReportsDashboardPanel'));
 const CalendarDashboardPanel = React.lazy(() => import('./CalendarDashboardPanel'));
 const MessagesPanel = React.lazy(() => import('./MessagesPanel'));
+const ProceduresDashboardPanel = React.lazy(() => import('./ProceduresDashboardPanel'));
+const ReportsDashboardPanel = React.lazy(() => import('./ReportsDashboardPanel'));
 const TaskCountPanel = React.lazy(() => import('./TaskCountPanel'));
 
 export class DashboardPanel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.websocketSubscriptions = [];
-        this.retryCount = 0;
-        this.connect = this.connect.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.websocketSubscriptions = [];
+    this.retryCount = 0;
+    this.connect = this.connect.bind(this);
+  }
 
-    connect = (user) => {
-        const uiEnv = this.props.appConfig.uiEnvironment.toLowerCase();
-        this.client = new Client({
-          debug: function (str) {
-            if (uiEnv === 'development' || uiEnv === 'local') {
-                console.log(str);
-            }
-          },
-          reconnectDelay: 5000,
-          heartbeatIncoming: 4000,
-          heartbeatOutgoing: 4000
-        });
+  connect = (user) => {
+    const uiEnv = this.props.appConfig.uiEnvironment.toLowerCase();
+    this.client = new Client({
+      debug: function (str) {
+        if (uiEnv === 'development' || uiEnv === 'local') {
+            console.log(str);
+        }
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000
+    });
 
-        const self = this;
-        this.client.webSocketFactory = function () {
-            return new SockJS(`${this.props.appConfig.workflowServiceUrl}ws/workflow/tasks?access_token=${self.props.kc.token}`);
-        };
-        this.client.onConnect = function(frame) {
-            self.props.log([{
-                message: 'Connected to websocket',
-                user: user,
-                level: 'info',
-                path: self.props.location.pathname
-            }]);
+    const self = this;
+    this.client.webSocketFactory = function () {
+        return new SockJS(`${this.props.appConfig.workflowServiceUrl}ws/workflow/tasks?access_token=${self.props.kc.token}`);
+    };
+    this.client.onConnect = function(frame) {
+      self.props.log([{
+        message: 'Connected to websocket',
+        user: user,
+        level: 'info',
+        path: self.props.location.pathname
+      }]);
 
-            const teamSub = self.client.subscribe(`/topic/task/${self.props.shift.get('teamid')}`, (msg) => {
-                PubSub.publishSync("refreshCount", {});
-            });
-            self.websocketSubscriptions.push(teamSub);
+      const teamSub = self.client.subscribe(`/topic/task/${self.props.shift.get('teamid')}`, (msg) => {
+        PubSub.publishSync("refreshCount", {});
+      });
+      self.websocketSubscriptions.push(teamSub);
 
-            const userSub = self.client.subscribe(`/user/queue/task`, (msg) => {
-                PubSub.publishSync("refreshCount", {});
-            });
-            self.websocketSubscriptions.push(userSub);
+      const userSub = self.client.subscribe(`/user/queue/task`, (msg) => {
+        PubSub.publishSync("refreshCount", {});
+      });
+      self.websocketSubscriptions.push(userSub);
 
-            self.props.log([{
-                message: 'Number of subscriptions ' + self.websocketSubscriptions.length,
-                user: user,
-                level: 'info',
-                path: self.props.location.pathname
-            }]);
-        };
-
-        this.client.onStompError = function (frame) {
-            self.props.log([{
-                message: `Failed to connect ${frame.headers['message']}`,
-                user: user,
-                level: 'error',
-                path: self.props.location.pathname
-            }]);
-        };
-        this.client.activate();
+      self.props.log([{
+        message: 'Number of subscriptions ' + self.websocketSubscriptions.length,
+        user: user,
+        level: 'info',
+        path: self.props.location.pathname
+      }]);
     };
 
-    componentDidMount() {
-        if (this.props.shift) {
-            const user = this.props.kc.tokenParsed.email;
-            this.connect(user);
-        }
-    }
+    this.client.onStompError = function (frame) {
+      self.props.log([{
+        message: `Failed to connect ${frame.headers['message']}`,
+        user: user,
+        level: 'error',
+        path: self.props.location.pathname
+      }]);
+    };
+    this.client.activate();
+  };
 
-    componentWillUnmount() {
-        this.retryCount = 0;
-        const user = this.props.kc.tokenParsed.email;
-        
-        if (this.client) {
-           this.client.deactivate(); 
-        }
+  componentDidMount() {
+    if (this.props.shift) {
+      const user = this.props.kc.tokenParsed.email;
+      this.connect(user);
     }
+}
 
-    render() {
-        return <div>
-            <div className="govuk-grid-row">
-                <ul className="govuk-list">
-                    <TaskCountPanel {...this.props}/>
-                    <MessagesPanel {...this.props} />
-                </ul>
-            </div>
-            <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible"/>
-            <div className="govuk-grid-row" style={{paddingTop: '10px'}}>
-                <Suspense
-                    fallback={<div style={{display: 'flex', justifyContent: 'center', paddingTop: '20px'}}><DataSpinner
-                        message="Loading panels..."/></div>}>
-                    <ul className="govuk-list">
-                    <ProceduresDashboardPanel {...this.props}/>
-                    <ReportsDashboardPanel {...this.props}/>
-                    <CalendarDashboardPanel {...this.props}/>
-                    </ul>
-                </Suspense>
-            </div>
+  componentWillUnmount() {
+    this.retryCount = 0;
+    const user = this.props.kc.tokenParsed.email;
+
+    if (this.client) {
+      this.client.deactivate();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="govuk-grid-row">
+          <ul className="govuk-list">
+            <TaskCountPanel {...this.props}/>
+            <MessagesPanel {...this.props} />
+          </ul>
         </div>
-    }
-
+        <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible"/>
+        <div className="govuk-grid-row" style={{paddingTop: '10px'}}>
+          <Suspense
+            fallback={<div style={{display: 'flex', justifyContent: 'center', paddingTop: '20px'}}><DataSpinner
+                message="Loading panels..."/></div>}>
+            <ul className="govuk-list">
+              <ProceduresDashboardPanel {...this.props}/>
+              <ReportsDashboardPanel {...this.props}/>
+              <CalendarDashboardPanel {...this.props}/>
+            </ul>
+          </Suspense>
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
 
 export default withRouter(connect((state) => {
-    return {
-        kc: state.keycloak,
-        appConfig: state.appConfig
-    }
+  return {
+    kc: state.keycloak,
+    appConfig: state.appConfig
+  }
 }, mapDispatchToProps)(withLog(DashboardPanel)));
