9a4ebba9b1efbd133dfe218db012de558a3aec0d
Pedro Miguel <pedro.miguel@live.co.uk>
2019-09-18 11:11:40 +0100

    Update submission banner test to use 'shallow'

--- app/core/components/SubmissionBanner.test.js
+++ app/core/components/SubmissionBanner.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import PubSub from 'pubsub-js';
 import SubmissionBanner from './SubmissionBanner';
 
 
 describe('SubmissionBanner', () => {
-  it('component is hidden if no submission', async () => {
-    const wrapper = await mount(<SubmissionBanner />);
+  it('component is hidden if no submission', () => {
+    const wrapper = shallow(<SubmissionBanner />);
     expect(wrapper.html()).toBeNull();
   });
-  it('renders submission if data received', async () => {
-    const wrapper = await mount(<SubmissionBanner />);
+
+  it('renders submission if data received', () => {
+    const wrapper = shallow(<SubmissionBanner />);
     PubSub.publishSync('submission', {
       submission: true,
       message: 'test',
     });
     expect(wrapper.html())
-      .toEqual('<div class="container" id="successfulSubmission" style="padding-top: 5px;"><div class="govuk-panel govuk-panel--confirmation"><div class="govuk-panel__body govuk-!-font-size-24 govuk-!-font-weight-bold">test</div></div></div>');
+      .toEqual('<div class="container" id="successfulSubmission" style="padding-top:5px"><div class="govuk-panel govuk-panel--confirmation"><div class="govuk-panel__body govuk-!-font-size-24 govuk-!-font-weight-bold">test</div></div></div>');
   });
 });
