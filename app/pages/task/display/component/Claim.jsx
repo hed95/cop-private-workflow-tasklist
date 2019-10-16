5983887f1382216663b092a23ccc9b4d51bcb5b2
Pedro Miguel <pedro.miguel@live.co.uk>
2019-10-16 16:58:15 +0100

    Linting

--- app/pages/task/display/component/Claim.jsx
+++ app/pages/task/display/component/Claim.jsx
@@ -1,43 +1,63 @@
-import React from "react";
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import {withRouter} from "react-router-dom";
-import * as actions from "../actions";
-import {claimSuccessful} from "../selectors";
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+// local imports
+import * as actions from '../actions';
+import { claimSuccessful } from '../selectors';
 
 export class Claim extends React.Component {
-
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.claimSuccessful) {
-            this.props.history.push(`/task/${this.props.task.get('id')}`);
-        }
+  componentDidUpdate(prevProps) {
+    if (prevProps.claimSuccessful) {
+      const { history, task } = this.props;
+      history.push(`/task/${task.get('id')}`);
     }
+  }
 
-    render() {
-        const {task, kc} = this.props;
-        const userId = kc.tokenParsed.email;
-        const taskAssignee = task.get('assignee');
-        const displayButton = taskAssignee === null || (taskAssignee !== userId);
-        return displayButton ? <button id="claimTask" className="govuk-button" type="submit"
-                      onClick={() =>
-                          this.props.claimTask(this.props.task.get('id'))} >Claim</button> : <div/>
+  render() {
+    const { claimTask, task, kc } = this.props;
+    const userId = kc.tokenParsed.email;
+    const taskAssignee = task.get('assignee');
+    const displayButton = taskAssignee === null || (taskAssignee !== userId);
+
+    function renderClaimButton() {
+      if (displayButton) {
+        return (
+          <button id="claimTask" className="govuk-button" type="submit" onClick={() => claimTask(task.get('id'))}>
+            Claim
+          </button>
+        );
+      }
+      return <React.Fragment></React.Fragment>;
     }
-
+    return ({ renderClaimButton });
+  }
 }
 
 Claim.propTypes = {
-    claimTask: PropTypes.func.isRequired,
-    claimSuccessful: PropTypes.bool
+  claimTask: PropTypes.func.isRequired,
+  claimSuccessful: PropTypes.bool,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  kc: PropTypes.shape({
+    tokenParsed: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  task: ImmutablePropTypes.map.isRequired,
 };
 
+Claim.defaultProps = {
+  claimSuccessful: false,
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
-export default withRouter(connect((state) => {
-    return {
-        kc: state.keycloak,
-        claimSuccessful: claimSuccessful(state)
-    }
-}, mapDispatchToProps)(Claim))
+export default withRouter(connect(state => ({
+  kc: state.keycloak,
+  claimSuccessful: claimSuccessful(state),
+}), mapDispatchToProps)(Claim));
