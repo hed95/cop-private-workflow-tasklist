1b2d685a8d773ae0dceb7af6238d0bc79e80b1dd
Pedro Miguel <pedro.miguel@live.co.uk>
2019-10-28 16:51:38 +0000

    Linting

--- app/pages/task/display/component/Attachments.jsx
+++ app/pages/task/display/component/Attachments.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const Attachments = () => {
-
-        return <div>
-            <div className="data">
-                <span className="data-item bold-medium">0 attachments</span>
-            </div>
-        </div>
-}
+const Attachments = () => (
+  <div>
+    <div className="data">
+      <span className="data-item bold-medium">0 attachments</span>
+    </div>
+  </div>
+);
 
 export default Attachments;
