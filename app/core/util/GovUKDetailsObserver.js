a3052dd4f64099b36255e1a9117f3af559f4c4ae
Amin Mohammed-Coleman <aminmc@gmail.com>
2020-02-24 11:09:19 +0000

    Subtree (#216)
    * fix complete workflow
    
    * updated subtree

--- app/core/util/GovUKDetailsObserver.js
+++ app/core/util/GovUKDetailsObserver.js
@@ -5,14 +5,14 @@ export default class GovUKDetailsObserver {
     this.node = node;
   }
 
-  create() {
+  create(subtree = false) {
     this.observer = new MutationObserver(() => {
       this.node.querySelectorAll('[data-module="govuk-details"]')
         .forEach(element => {
           new Details(element).init()
         });
     });
-    this.observer.observe(this.node, { childList: true, attributes: false });
+    this.observer.observe(this.node, { childList: true, attributes: false, subtree: subtree });
     return this;
   }
 
