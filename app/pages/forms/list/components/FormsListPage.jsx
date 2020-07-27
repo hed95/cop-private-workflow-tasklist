2d11b21465c922c5e445e11bfe65a15a6f0780c4
JenniKate Wallace <jennikate@gmail.com>
2020-07-27 16:04:15 +0100

    Update page layout to on third, two thirds

--- app/pages/forms/list/components/FormsListPage.jsx
+++ app/pages/forms/list/components/FormsListPage.jsx
@@ -39,23 +39,24 @@ export class FormsListPage extends React.Component {
     const { isFetchingProcessDefinitions, processDefinitions } = this.props;
     return (
       <React.Fragment>
-
-        <div className="govuk-grid-row" id="proceduresCountLabel">
-          <div className="govuk-grid-column-one-half">
+        <div className="govuk-grid-row">
+          <div className="govuk-grid-column-two-thirds">
             <h1 className="govuk-heading-xl">Forms</h1>
-            <h2 className="govuk-heading-l">Operational forms</h2>
-            <p className="govuk-body-l">
-              Here are all the forms available on COP for completing a variety of tasks from reporting events to requesting building passes.
-            </p>
           </div>
         </div>
+
         <div className="govuk-grid-row">
-          <div className="govuk-grid-column-full">
+          <div className="govuk-grid-column-one-third">
+            <h3 className="govuk-heading-m">Filter list</h3>
+          </div>
+          <div className="govuk-grid-column-two-thirds">
+            <h2 className="govuk-heading-l">Operational forms</h2>
+            <p className="govuk-body-l">Here are all the forms available on COP for completing a variety of tasks from reporting events to requesting building passes.</p>
             {isFetchingProcessDefinitions && (
-              <h4 className="govuk-heading-s" id="loading">
-                Loading forms...
-              </h4>
-            )}
+            <h4 className="govuk-heading-s" id="loading">
+                  Loading forms...
+            </h4>
+              )}
             {processDefinitions.map(p => {
                 const processDefinitionKey = p.getIn([
                   'process-definition',
@@ -81,35 +82,6 @@ export class FormsListPage extends React.Component {
                       >
                         {description}
                       </a>
-                      {/* <div
-                        className="govuk-grid-row"
-                        key={processDefinitionKey}
-                      >
-                        <div className="govuk-grid-column-one-third">
-                          <button
-                            id="actionButton"
-                            className="govuk-button govuk-button--start"
-                            onClick={() => this.process(p)}
-                            type="submit"
-                          >
-                            Start
-                            <svg
-                              className="govuk-button__start-icon"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="17.5"
-                              height="19"
-                              viewBox="0 0 33 40"
-                              role="presentation"
-                              focusable="false"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M0 0h13l20 20-20 20H0l20-20z"
-                              />
-                            </svg>
-                          </button>
-                        </div>
-                      </div> */}
                     </div>
                   </div>
                 );
