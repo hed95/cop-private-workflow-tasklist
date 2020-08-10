eeeb8448725492499e8fad34c63572b1e0e02238
JenniKate Wallace <jennikate@gmail.com>
2020-08-10 15:48:43 +0100

    Update priority to handle bands rather than static numbers
    Tidy up

--- app/core/util/priority.js
+++ app/core/util/priority.js
@@ -1,14 +1,9 @@
 export const priority = priority => {
   let result = null;
-  switch (priority) {
-    case 50:
-      result = 'Low';
-      break;
-    case 100:
-      result = 'Medium';
-      break;
-    default:
-      result = 'High';
-  }
+
+  if (priority <= 50) { result = 'Low' }
+  else if (priority > 50 && priority < 150) { result = 'Medium' }
+  else if (priority >= 150) { result = 'High' }
+  else { result = 'High' }
   return result;
 };
--- app/pages/tasks/components/TaskUtils.js
+++ app/pages/tasks/components/TaskUtils.js
@@ -32,35 +32,34 @@ export default class TaskUtils {
     }
 
     applyGrouping(groupBy, tasks) {
-        switch (groupBy) {
-            case 'reference':
-                const byReference = _.groupBy(tasks, data => {
-                    return data.businessKey;
-                });
-                const sortKeys = _.orderBy(Object.keys(byReference), o => {
-                    return moment(o.split('-')[1]).format('YYYYMMDD');
-                }, ['desc']);
+      switch (groupBy) {
+        case 'reference':
+          const byReference = _.groupBy(tasks, data => {
+            return data.businessKey;
+          });
+          const sortKeys = _.orderBy(Object.keys(byReference), o => {
+            return moment(o.split('-')[1]).format('YYYYMMDD');
+          }, ['desc']);
+        return _.fromPairs(_.map(sortKeys, key => [key, byReference[key]]));
+        
+        case 'priority':
+          const byPriority = _.groupBy(tasks, item => {
+            return priority(Number(` ${item.task.priority}`))
+          });
+          const sortByPriority = ['High', 'Medium', 'Low'];
+          return _.fromPairs(_.map(sortByPriority, key => {
+            return [key, byPriority[key]]
+          }));
 
-                return _.fromPairs(_.map(sortKeys, key => [key, byReference[key]]));
-            case 'priority':
-                const byPriority = _.groupBy(tasks, data => {
-                    return data.task.priority;
-                });
-                const sortByPriority = _.orderBy(Object.keys(byPriority), key => {
-                    return Number(key);
-                }, ['desc']);
-                return _.fromPairs(_.map(sortByPriority, key => {
-                    return [priority(Number(` ${  key}`)).trim(), byPriority[key]]
-                }));
-            default:
-                const sortByKeys = object => {
-                    const sort = _.orderBy(Object.keys(object), [key => key.toLowerCase()], ['asc']);
-                    return _.fromPairs(_.map(sort, key => [key, object[key]]));
-                };
-                return sortByKeys(_.groupBy(tasks, data => {
-                    return data['process-definition'] ? data['process-definition'].category : 'Other';
-                }));
-        }
+        default:
+          const sortByKeys = object => {
+            const sort = _.orderBy(Object.keys(object), [key => key.toLowerCase()], ['asc']);
+            return _.fromPairs(_.map(sort, key => [key, object[key]]));
+          };
+        return sortByKeys(_.groupBy(tasks, data => {
+          return data['process-definition'] ? data['process-definition'].category : 'Other';
+        }));
+      }
     }
 
     generateCaption(grouping, val) {
