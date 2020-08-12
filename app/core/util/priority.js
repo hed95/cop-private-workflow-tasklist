451702e6f6a3d9f8586801dc5bcad8fe3015acc5
JenniKate Wallace <jennikate@gmail.com>
2020-08-12 12:38:02 +0100

    Update priority to use constants and bands
    addition

--- app/core/util/priority.js
+++ app/core/util/priority.js
@@ -1,14 +1,18 @@
+/* eslint-disable import/prefer-default-export */
+import AppConstants from '../../common/AppConstants';
+
 export const priority = priority => {
+  const high = AppConstants.HIGH_PRIORITY_LABEL;
+  const medium = AppConstants.MEDIUM_PRIORITY_LABEL;
+  const low = AppConstants.LOW_PRIORITY_LABEL;
+  const valueHigh = AppConstants.HIGH_PRIORITY_LOWER_LIMIT;
+  const valueLow = AppConstants.LOW_PRIORITY_UPPER_LIMIT;
   let result = null;
-  switch (priority) {
-    case 50:
-      result = 'Low';
-      break;
-    case 100:
-      result = 'Medium';
-      break;
-    default:
-      result = 'High';
-  }
+
+  if (priority <= valueLow) { result = low }
+  else if (priority > valueLow && priority < valueHigh) { result = medium }
+  else if (priority >= valueHigh) { result = high }
+  else { result = medium }
+
   return result;
 };
