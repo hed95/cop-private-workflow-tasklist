ca959d8d5c1506413dfecfdf965a1ebd9cb51ef5
JenniKate Wallace <jennikate@gmail.com>
2020-08-12 12:19:34 +0100

    Add constants for priorities
    Adjust constants to cater to second scenario

--- app/common/AppConstants.js
+++ app/common/AppConstants.js
@@ -1,6 +1,8 @@
 export default class AppConstants {
+  // Names
   static APP_NAME = 'Central Operations Platform';
 
+  // App paths
   static CALENDAR_PATH = '/calendar';
 
   static CASES_PATH = '/cases';
@@ -11,13 +13,9 @@ export default class AppConstants {
 
   static MESSAGES_PATH = '/messages';
 
-  static MOBILE_WIDTH = 640;
-
   static ONBOARD_USER_PATH = '/onboard-user';
 
   static PROCEDURE_DIAGRAM_PATH = '/procedure-diagram';
-
-  static REFRESH_TIMEOUT = 300000;
 
   static REPORT_PATH = '/report';
 
@@ -32,4 +30,24 @@ export default class AppConstants {
   static YOUR_GROUP_TASKS_PATH = '/your-group-tasks';
 
   static YOUR_TASKS_PATH = '/your-tasks';
+
+  // Priorities
+  static HIGH_PRIORITY_LABEL = 'High';
+
+  static HIGH_PRIORITY_LOWER_LIMIT = 150;
+
+  static LOW_PRIORITY_LABEL = 'Low';
+
+  static LOW_PRIORITY_UPPER_LIMIT = 50;
+
+  static MEDIUM_PRIORITY_LABEL = 'Medium';
+
+  static MEDIUM_PRIORITY_LOWER_LIMIT = 100;
+
+  // Sizes
+  static MOBILE_WIDTH = 640;
+
+  // Timers
+  static REFRESH_TIMEOUT = 300000;
+
 }
