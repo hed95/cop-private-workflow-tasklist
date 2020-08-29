cb6b4135f32854ec15f6f2cfe9aa621a8a88d394
Gavin Orland <gavin.orland@gmail.com>
2020-08-29 21:11:44 +0100

    Fix no-shadow
    Also reformat.

--- app/core/util/priority.js
+++ app/core/util/priority.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import AppConstants from '../../common/AppConstants';
 
-export const priority = priority => {
+export const priority = p => {
   const high = AppConstants.HIGH_PRIORITY_LABEL;
   const medium = AppConstants.MEDIUM_PRIORITY_LABEL;
   const low = AppConstants.LOW_PRIORITY_LABEL;
@@ -9,10 +9,15 @@ export const priority = priority => {
   const valueLow = AppConstants.LOW_PRIORITY_UPPER_LIMIT;
   let result = null;
 
-  if (priority <= valueLow) { result = low }
-  else if (priority > valueLow && priority < valueHigh) { result = medium }
-  else if (priority >= valueHigh) { result = high }
-  else { result = medium }
+  if (p <= valueLow) {
+    result = low;
+  } else if (p > valueLow && p < valueHigh) {
+    result = medium;
+  } else if (p >= valueHigh) {
+    result = high;
+  } else {
+    result = medium;
+  }
 
   return result;
 };
